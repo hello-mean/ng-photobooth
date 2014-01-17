c32b1d58e1e978b4e30b1843fb8106f28f9d1c72
Brian Scaturro <scaturrob@gmail.com>
2014-01-16 22:42:26 -0500

    separate photoPreview tests by event

--- test/unit/photoPreview.spec.js
+++ test/unit/photoPreview.spec.js
@@ -21,43 +21,47 @@ describe('photo-preview', function() {
     element = $compile(prev)($scope);
   });
 
-  it("should replace element html with video and canvas on photobooth:start event", function() {
-    $scope.$emit('photobooth:start');
-    var video = element.find('video'),
-        canvas = element.find('canvas');
-    expect(video.length).toBe(1);
-    expect(canvas.length).toBe(1);
+  describe('photobooth:start event', function() {
+    it("should replace element html with video and canvas on photobooth:start event", function() {
+      $scope.$emit('photobooth:start');
+      var video = element.find('video'),
+          canvas = element.find('canvas');
+      expect(video.length).toBe(1);
+      expect(canvas.length).toBe(1);
+    });
+
+    it("should place the autoplay attribute on the video on photobooth:start event", function() {
+      $scope.$emit('photobooth:start');
+      var video = element.find('video');
+      expect(video.attr('autoplay')).toBeTruthy();
+    });
+
+    it("should call webcam.start on photobooth:start", function() {
+      spyOn(camera, "start");
+      $scope.$emit('photobooth:start');
+      expect(camera.start).toHaveBeenCalled();
+    });
+
+    it("should set the element class to the camera-on-class attr on photobooth:start", function() {
+      $scope.$emit('photobooth:start');
+      expect(element.hasClass('is-on')).toBe(true);
+    });
+
+    it("should emit a photobooth:cameraStart event with the video when the camera starts", function() {
+      spyOn($scope, '$emit').andCallThrough();
+      $scope.$emit('photobooth:start');
+      expect($scope.$emit.mostRecentCall.args[0]).toBe('photobooth:cameraStart');
+      expect($scope.$emit.mostRecentCall.args[1]).toEqual(element.find('video')[0]);
+    });
+
+    it("should trigger the photobooth:cameraError event when there is a camera error", function() {
+      spyOn(camera, 'start').andCallFake(function(v, s, e) { e('error'); });
+      spyOn($scope, '$emit').andCallThrough();
+      $scope.$emit('photobooth:start');
+      expect($scope.$emit.mostRecentCall.args[0]).toBe('photobooth:cameraError');
+      expect($scope.$emit.mostRecentCall.args[1]).toBe('error');
+    });
+   
   });
 
-  it("should place the autoplay attribute on the video on photobooth:start event", function() {
-    $scope.$emit('photobooth:start');
-    var video = element.find('video');
-    expect(video.attr('autoplay')).toBeTruthy();
-  });
-
-  it("should call webcam.start on photobooth:start", function() {
-    spyOn(camera, "start");
-    $scope.$emit('photobooth:start');
-    expect(camera.start).toHaveBeenCalled();
-  });
-
-  it("should set the element class to the camera-on-class attr on photobooth:start", function() {
-    $scope.$emit('photobooth:start');
-    expect(element.hasClass('is-on')).toBe(true);
-  });
-
-  it("should emit a photobooth:cameraStart event with the video when the camera starts", function() {
-    spyOn($scope, '$emit').andCallThrough();
-    $scope.$emit('photobooth:start');
-    expect($scope.$emit.mostRecentCall.args[0]).toBe('photobooth:cameraStart');
-    expect($scope.$emit.mostRecentCall.args[1]).toEqual(element.find('video')[0]);
-  });
-
-  it("should trigger the photobooth:cameraError event when there is a camera error", function() {
-    spyOn(camera, 'start').andCallFake(function(v, s, e) { e('error'); });
-    spyOn($scope, '$emit').andCallThrough();
-    $scope.$emit('photobooth:start');
-    expect($scope.$emit.mostRecentCall.args[0]).toBe('photobooth:cameraError');
-    expect($scope.$emit.mostRecentCall.args[1]).toBe('error');
-  });
 });
